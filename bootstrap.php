66f17f876c02fe594fc87eb2d3c89136019ee763
Wil Moore III <wil.moore@wilmoore.com>
2013-05-08 01:12:20 -0600

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -10,8 +10,8 @@ require 'vendor/autoload.php';
 error_reporting(-1);
 
 // cli apps can log to STDOUT
-ini_set('error_log', 'stdout');
-ini_set('html_errors', false);
+ini_set('error_log',      'stdout');
+ini_set('html_errors',    false);
 ini_set('display_errors', true);
 
 // UTC all the things
