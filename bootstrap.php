32073142501141820513cbe52740c03f5067765e
Wil Moore III <wil.moore@wilmoore.com>
2013-05-08 01:11:39 -0600

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -7,17 +7,14 @@ require 'vendor/autoload.php';
 #############################################
 
 // strict error reporting
-
 error_reporting(-1);
 
 // cli apps can log to STDOUT
-
 ini_set('error_log', 'stdout');
 ini_set('html_errors', false);
 ini_set('display_errors', true);
 
 // UTC all the things
-
 ini_set('date.timezone',  'UTC');
 date_default_timezone_set('UTC');
 
